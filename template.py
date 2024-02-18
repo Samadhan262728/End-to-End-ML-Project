4ad62ee6d3e8d9ff2fdbdcb22e7b2a01ef03f846
Samadhan Pisal <samadhan.pisal.2828@gmail.com>
2024-02-18 17:19:04 +0530

    Create Project Template

---
+++ template.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path #Using pathlib's Path class provides a more readable and convenient way to work with file paths in Python code compared to traditional string manipulation.
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "mlproject"
+
+
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+
+]
+
+
+
+for filepath in list_of_files:
+    #print(filepath)
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath) # here we have separating file directory & file name from list of Files
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
+
